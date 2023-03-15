6049152c41aaf475f558a66a476417e4b2fa7f1a
Yilmer Palacios <ya.palacios@uniandes.edu.co>
2023-03-14 21:45:15 -0500

    Create Taller4.R

---
+++ Scripts/Taller4.R
@@ -0,0 +1,26 @@
+################################################################
+# Problem Set 4: Script
+# Authors: Yilmer Palacios, Betina Cortés, Lida Jimena Cárdenas,
+# Nelson Fabián López
+################################################################
+
+# Loading Libraries ----
+rm(list = ls()) 
+
+# install.packages("pacman")
+library("pacman")
+p_load("tidyverse",
+       "sf",
+       "naniar",
+       "tidymodels",
+       "readxl",
+       "psych",
+       "ranger",
+       "glmnet",
+       "naniar",
+       "tidyverse",
+       "caret",
+       "glmnet",
+       "ggplot2",
+       "ggraph",
+       "gt")
