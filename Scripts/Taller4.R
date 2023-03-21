83d0fed78fab052c563bff71bec2d51d455db80d
Lidariverac <Lj.rivera@uniandes.edu.co>
2023-03-20 19:27:53 -0500

    Update Taller4.R

--- Scripts/Taller4.R
+++ Scripts/Taller4.R
@@ -9,88 +9,27 @@ rm(list = ls())
 
 # install.packages("pacman")
 library("pacman")
-
-p_load("tm",
-       "stringi",
-       "proxy",
-       "tidyverse",
-       "sf",
-       "naniar",
-       "tidymodels",
-       "readxl",
-       "psych",
-       "ranger",
-       "glmnet",
-       "naniar",
-       "tidyverse",
-       "caret",
-       "glmnet",
-       "ggplot2",
-       "ggraph",
-       "gt")
-
-setwd("/Users/betinacortes/Desktop/Repositorio_Taller4")
-# setwd("C:/Users/Yilmer Palacios/Desktop/Repositorios GitHub/Repositorio_Taller4")
+p_load("tidyverse", "tidytext", "tm", "SnowballC", "topicmodels", 
+       "wordcloud", "igraph", "ggraph", "Matrix", "xgboost", "caret",
+       "syuzhet", "text2vec", "glmnet", "gt")
 
 
-# Importing Dataset ----
-# Removing City and operation type as they don't add information 
-train <- read.csv("stores/Data_Kaggle/train.csv",header = TRUE,sep=",")
-test <- read.csv("stores/Data_Kaggle/test.csv",header = TRUE,sep=",")
 
-text <- stri_trans_general(str = train$text, id = "Latin-ASCII")
+#setwd("/Users/betinacortes/Desktop/Repositorio_Taller4")
+setwd("C:/Users/Yilmer Palacios/Desktop/Repositorios GitHub/Repositorio_Taller4")
 
+#Cargamos bases de datos
 
-texts<-VCorpus(VectorSource(train$text))
-texts
+train <- read_csv("Stores/Data_Kaggle/train.csv") |> 
+  replace_na(list(text = ".")) |> 
+  mutate(len = str_count(text))
 
-rm("dtm_idf") 
-
-#limpiamos el corpus texts
-texts <- tm_map(texts, removePunctuation)
-texts <- tm_map(texts, content_transformer(tolower))
-texts <- tm_map(texts, removeWords, stopwords("spanish"))
-texts <- tm_map(texts,content_transformer(removeNumbers))
-texts<-tm_map(texts,content_transformer(stripWhitespace))
-texts <- tm_map(texts, content_transformer(gsub), pattern = "[!|?|¡|¿|“|”|´|´|‘|’]", replacement = "")
-texts <- tm_map(texts, content_transformer(gsub), pattern = "\\b[0-9]+\\b", replacement = "")
-texts <- tm_map(texts, content_transformer(gsub), pattern = "\"", replacement = "")
-texts <- tm_map(texts, content_transformer(function(x) stri_replace_all_regex(x, "\\p{Emoji}", "")))
+test <- read_csv("Stores/Data_Kaggle/test.csv") |> 
+  replace_na(list(text = ".")) |> 
+  mutate(len = str_count(text))
 
 
-dtm_idf<-DocumentTermMatrix(texts,
-                            control=list(weighting=weightTfIdf,
-                            stopwords=c("con", "si")))
-
-dtm_idf <- removeSparseTerms(dtm_idf, sparse = 0.95)
-inspect(dtm_idf[100:103,])
-
-dtm_idf
-inspect(dtm_idf)
-
-#revisamos frecuencias
-terms_freq <- findFreqTerms(dtm_idf, lowfreq = 1)
-freq_table <- data.frame(term = terms_freq, freq = colSums(as.matrix(dtm_idf[,terms_freq])))
-print(freq_table)
-
-## Alternativa 2!!
-
-# Loading Libraries ----
-rm(list = ls()) 
-
-# install.packages("pacman")
-library("pacman")
-p_load("tidyverse", "textir", "wordcloud", "tidytext", "tm",
-       "SnowballC", "parsnip", "yardstick", "gt", "maptpx", "gamlr")
-
-# Importing Dataset ----
-# Loading Training and test dataset
-
-train |> 
-  mutate(longitud = str_length(text)) |> 
-  arrange(longitud)
-
-## Descriptive statistics
+## Tabla de estadisticas descriptivas
 
 num_tweets <- train |> 
   group_by(name) |> 
@@ -99,12 +38,13 @@ num_tweets <- train |>
 
 longitud_tweets <- train |> 
   group_by(name) |> 
-  summarise(longitud = mean(str_length(text), na.rm = T))
+  summarise(longitud = median(str_length(text), na.rm = T))
 
 contiene_hashtags <- train |> 
   group_by(name) |>
   mutate(tiene_hashtag = str_detect(text, "#")) |> 
   summarise(hashtag = mean(tiene_hashtag, na.rm = T))
+
 
 tabla_resumen <- num_tweets |> 
   left_join(longitud_tweets) |> 
@@ -131,9 +71,59 @@ tabla_resumen <- num_tweets |>
     hashtag ~ px(120)
   ) 
 
-gtsave(tabla_resumen, "Tablas/tabla_resumen.html")
+gtsave(tabla_resumen, "Views/tabla_resumen.html")
 
-## Creating Wordcouds based on the train dataset.
+
+## Visualizamos número de caracteres para determinar tiempo de actividad en la Twitter
+train |> 
+  group_by(name) |> 
+  summarise(CountMedian = median(len, na.rm = TRUE)) |> 
+  ungroup() |> 
+  mutate(author = reorder(name, CountMedian)) |> 
+  ggplot(aes (x = author, y = CountMedian)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(x = name, y = CountMedian + 15, label = CountMedian)) +
+  labs(
+    title = "Mediana de la longitud de caracteres\nde los tweets por autor",
+    x = "Autor", 
+    y = "Mediana") +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "white", color = "white")
+  )
+
+ggsave("Views/Numero_caracteres.png", width = 1500, height = 1500, units = "px")
+
+
+
+## Densidad del número de caractéres por político ----
+
+train |> 
+  ggplot(aes(x = len)) +
+  geom_histogram() +
+  labs(
+    title = "Distribución de longitud de tweets por autor",
+    y = "Conteo", 
+    x = "Número de caracteres") +
+  facet_wrap(~name) +
+  scale_x_continuous(limits = c(15, 280)) +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "white", color = "white")
+  )
+
+ggsave("Views/Numero_caracteres_histograma.png", width = 1500, height = 1500, units = "px")
+
+
+## Tokenización ----
+
+train |> 
+  unnest_tokens(output = "word", input = text, token = "words") |> 
+  anti_join(tibble(word = stopwords(kind = "es")))
+
+
+
+## Creación de nubes de palabras ----
 
 bag_of_words <- train |> 
   unnest_tokens(output = "word", input = text, token = "words") |> 
@@ -157,80 +147,58 @@ crear_wordcloud <- function(dataset, vector_colors, seed = 42) {
             colors = colorRampPalette(colors = vector_colors)(3))
 }
 
-jpeg("Wordclouds/wordcloud_lopez.jpeg", width = 800, height = 800)
+
+jpeg("Views/wordcloud_lopez.jpeg", width = 800, height = 800)
 crear_wordcloud(bag_of_words_lopez, c("#74c476", "#006d2c"))
 dev.off()
 
-jpeg("Wordclouds/wordcloud_uribe.jpeg", width = 800, height = 800)
+jpeg("Views/wordcloud_uribe.jpeg", width = 800, height = 800)
 crear_wordcloud(bag_of_words_uribe, c("#6baed6", "#08519c"))
 dev.off()
 
-jpeg("Wordclouds/wordcloud_petro.jpeg", width = 800, height = 800)
+jpeg("Views/wordcloud_petro.jpeg", width = 800, height = 800)
 crear_wordcloud(bag_of_words_petro, c("#9e9ac8", "#54278f"))
 dev.off()
 
-## Tokenizing and lemmatizing the dataset
 
-test_new <- test |> 
-  bind_cols(name = as.character(NA)) |> 
-  select(id, name, text)
+### TF-IDF ----
 
-train_test <- train |> 
-  bind_rows(test_new)
+## Hacemos conteo de la aparición de las palabras para cada político
+## Cabe resaltar que aun no se ha aplicado el stopword
+trainWords <- train |> 
+  unnest_tokens(output = word, input = text) |> 
+  count(name, word, sort = TRUE)
 
-train_test_tokens <- train_test |> 
-  unnest_tokens(output = "word", input = text, token = "words", drop = F) |> 
-  anti_join(tibble(word = stopwords(kind = "es"))) |> 
-  filter(str_length(word) >= 3) |> 
-  mutate(word = wordStem(word, language = "es"))
 
-train_test_matrix <- train_test_tokens |>  
-  count(id, word) |> 
-  cast_dtm(document = id, term = word, value = n, weighting = tm::weightTfIdf)
+## Calculamos total de palabras por autor.
+totalWords <- trainWords |> 
+  group_by(name) |> 
+  summarize(total = sum(n))
 
-train_test_matrix_less_sparse <- removeSparseTerms(train_test_matrix, sparse = .9997)
+## Agregamos el numero total de palabras a la data original para construir TFIDF
+trainWords <- trainWords |>  left_join(totalWords) |> 
+  filter(!is.na(name)) |> 
+  bind_tf_idf(word, name, n)
 
-train_matrix_less_sparse <- train_test_matrix_less_sparse |> 
-  tidy() |> 
-  filter(document %in% train$id) |> 
-  cast_dtm(document = document, term = term, value = count)
+plot_trainWords <- trainWords |> 
+  arrange(desc(tf_idf)) |> 
+  mutate(word = factor(word, levels = rev(unique(word))))
 
-test_matrix_less_sparse <- train_test_matrix_less_sparse |> 
-  tidy() |> 
-  complete(document, term) |> 
-  replace_na(list(count = 0)) |> 
-  filter(document %in% test$id) |> 
-  cast_dtm(document = document, term = term, value = count)
 
-train_filtered <- train_test_tokens |> 
-  distinct(id, name, text) |> 
-  filter(id %in% train$id)
+## Graficamos palabras con mayor TFIDF
+plot_trainWords |> 
+  top_n(20) |> 
+  ggplot(aes(word, tf_idf)) +
+  geom_col() +
+  labs(title = "Top 20 palabras por TF-IDF", x = NULL, y = "TF-IDF") +
+  coord_flip() +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.title.position = "plot",
+    plot.background = element_rect(fill = "white", color = "white")
+  )
 
-test_filtered <- train_test_tokens |> 
-  distinct(id, name, text) |> 
-  filter(id %in% test$id)
-
-## Split into train_split and validation_split
-set.seed(42)
-sample_size <- floor(0.75 * nrow(train_matrix_less_sparse))
-train_ind <- sample(nrow(train_matrix_less_sparse), size = sample_size)
-
-train_split <- train_matrix_less_sparse[train_ind, ]
-validation_split <- train_matrix_less_sparse[-train_ind, ]
-
-## Models - Random forest
-rf_classifier <- rand_forest() |> 
-  set_engine("ranger") |> 
-  set_mode("classification")
-
-rf_fit <- rf_classifier |> 
-  fit_xy(x = as.data.frame(as.matrix(train_split)),
-         y = as.factor(train_filtered$name[train_ind]))
-
-rf_validation_pred <- predict(rf_fit,
-                              new_data = as.data.frame(as.matrix(validation_split))) |> 
-  bind_cols(predict(rf_fit, as.data.frame(as.matrix(validation_split)), type= "prob"))
-
-rf_validation_pred
+ggsave("Views/Top_20_TFIDF.png", width = 1500, height = 1500, units = "px")
 
 
